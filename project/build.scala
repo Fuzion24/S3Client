7f5bc886d603ddcd49713ea990ea83b08f8a8fe0
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-07-31 15:34:03 -0400

    Build into a .jar

--- project/build.scala
+++ project/build.scala
@@ -2,6 +2,8 @@ import sbt._
 import Keys._
 import com.github.hexx.GithubRepoPlugin._
 import com.typesafe.sbt.SbtGit.GitKeys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object Publish {
   val organization = "aws"
@@ -17,17 +19,51 @@ object Versions {
 }
 
 object AWSS3 extends Build {
-  lazy val application = Project("AWS-S3", file("."), settings = aws_s3_settings)
+  val projectName = "S3Client"
 
-   lazy val aws_s3_settings = Defaults.defaultSettings ++ githubRepoSettings ++ Seq(
-    name         := "S3Client",
-    version      := Publish.version,
-    organization := Publish.organization,    
-    localRepo    := Publish.localRepo,
-    githubRepo   := Publish.githubRepo,
-    scalaVersion := Versions.scala,
+  lazy val application = Project(projectName, file("."), settings = aws_s3_settings)
+
+  lazy val aws_s3_settings = 
+    Defaults.defaultSettings ++ 
+    githubRepoSettings ++ 
+    assemblySettings ++
+    Seq(
+      name         := projectName,
+      version      := Publish.version,
+      organization := Publish.organization,    
+      localRepo    := Publish.localRepo,
+      githubRepo   := Publish.githubRepo,
+      scalaVersion := Versions.scala,
+      jarName in assembly := projectName + ".jar",
+      mainClass in assembly := Some("com.apk.service.web.Standalone"),
+      mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+      {
+        case "application.conf" => MergeStrategy.concat
+        case "reference.conf"   => MergeStrategy.concat
+        case "mime.types"       => MergeStrategy.filterDistinctLines
+        case PathList("org", "hamcrest", _ @ _*) => MergeStrategy.first
+        case PathList("com", "google", "common", _ @ _*) => MergeStrategy.first
+        case PathList("org", "xmlpull", _ @ _*) => MergeStrategy.first
+        case PathList(ps @ _*) if ps.last.toLowerCase.startsWith("notice") ||
+                                  ps.last.toLowerCase == "license" || 
+                                  ps.last.toLowerCase == "license.txt" || 
+                                  ps.last.toLowerCase == "asm-license.txt" ||
+                                  ps.last.endsWith(".html") => MergeStrategy.rename 
+        case PathList("META-INF", xs @ _*) =>
+          (xs map {_.toLowerCase}) match {
+            case ("manifest.mf" :: Nil) | ("index.list" :: Nil) | ("dependencies" :: Nil) =>
+              MergeStrategy.discard
+            case ps @ (x :: xs) if ps.last.endsWith(".sf") || ps.last.endsWith(".dsa")  =>
+              MergeStrategy.discard
+            case "services" :: xs =>
+              MergeStrategy.filterDistinctLines
+            case _ => MergeStrategy.deduplicate
+          }
+        case _ => MergeStrategy.deduplicate
+      }
+    },
     libraryDependencies ++= Seq(
-	  "net.databinder.dispatch" %% "dispatch-core" % "0.10.1"	 
+	    "net.databinder.dispatch" %% "dispatch-core" % "0.10.1"	 
     )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 resolvers ++= Seq("fuzion24 Releases" at "http://fuzion24.github.io/maven/releases")
 
 addSbtPlugin("com.github.hexx" % "sbt-github-repo" % "0.1.0")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.7")
