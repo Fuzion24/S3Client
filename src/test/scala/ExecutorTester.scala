61b99121709a71da25fe2e5b11147aeb768fcdc2
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-08-06 18:52:52 -0400

    Add integration test to git ignore for now

--- src/test/scala/ExecutorTester.scala
+++
@@ -1,8 +0,0 @@
-import net.aws.s3._
-import org.scalatest.FunSuite
-import scala.concurrent.{ExecutionContext, Await}
-import scala.concurrent.duration._
-
-class ExecutorTester extends FunSuite {
-
-}
