c82e94105e3a607f09d7a3420e13bd84076c971b
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-08-06 18:50:41 -0400

    Add date parser for S3 results

--- src/main/scala/net/aws/s3/DateHelper.scala
+++ src/main/scala/net/aws/s3/DateHelper.scala
@@ -6,3 +6,8 @@ import java.util.{SimpleTimeZone, Locale}
 object RFC822DateParser extends SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US) {
   this.setTimeZone(new SimpleTimeZone(0, "GMT"))
 }
+
+//Example Date: "2013-05-14T21:21:15.000Z"
+object S3ResultDate extends SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US) {
+  this.setTimeZone(new SimpleTimeZone(0, "GMT"))
+}
